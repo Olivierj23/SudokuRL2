d4cc16d3d213b911af0f4fda1f7540a36366a752
Olivierj23 <122296008+Olivierj23@users.noreply.github.com>
2024-02-26 16:54:42 -0500

    stuff changed

--- agent.py
+++ agent.py
@@ -11,7 +11,10 @@ LR = 0.001
 
 class Agent:
     def __init__(self):
-        pass
+        self.n_games = 0
+        self.epsilon = 0
+        self.gamma = 0
+        self.memory = deque(maxlen=MAX_Memory)
 
     def get_state(self, game):
         pass
@@ -22,7 +25,7 @@ class Agent:
     def train_long_memory(self):
         pass
 
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, done):
         pass
 
     def get_action(self, state):
@@ -30,7 +33,40 @@ class Agent:
 
 
 def train():
-    pass
+    plot_number_solved = []
+    plot_average_number_solved = []
+    record = 0
+    agent = Agent()
+    game = SudokuGameAI
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+
+        # get move
+        final_move = agent.get_action(state_old)
+
+        # perform move and get new state
+        reward, done = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            # train long memory, plot result
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if agent.n_games % 10
+
+
+
+
+
 
 if __name__ == "__main__":
     train()
